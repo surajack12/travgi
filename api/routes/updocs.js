1c379420d3bd5f532f1f34c9765d629fbbd0e5d4
surajack12 <sahusooraj2619@gmail.com>
2020-09-28 20:55:49 +0530

    changes in api folder

--- api/routes/updocs.js
+++ api/routes/updocs.js
@@ -1,1 +1,34 @@
+var express = require('express');
+var router = express();
+var multer = require('multer');
+var mysql = require('mysql')
+var bodyParse = require('body-parser')
+
+
+
+
+var conn =mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'artifexcompletedb'
+ });
+
+
+ Router.use(bodyParser.json());
+ Router.use(bodyParser.urlencoded({ extended: false })); 
+
+ 
+
+var conn =mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'artifexcompletedb'
+ });
+
+
+ Router.use(bodyParser.json());
+ Router.use(bodyParser.urlencoded({ extended: false })); 
+
 
